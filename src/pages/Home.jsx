63dfeee308d5cf41bb2278e372cd49caaf858db7
Prince Caleb <akakpo.c.benjamin@gmail.com>
2023-03-01 00:05:56 +0000

    Speech to text conversion + Send+Get Server Data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import { Heading, Text, Box, Input, IconButton } from '@chakra-ui/react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Heading, Text, Box, Input, IconButton, Button } from '@chakra-ui/react'
  
 export function Home(){
 
@@ -15,14 +17,78 @@ export function Home(){
         }
     }
 
+    const [transcription, setTranscription] = useState('');
+    const [serverData, setServerData] = useState('');
+
+    const fetchServerData = async () => {
+        try {
+        const response = await axios.get('http://localhost:5000/data');
+        setServerData(response.data);
+        } catch (error) {
+        console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchServerData();
+    }, []);
+
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = 'en-US';
+
+    recognition.onresult = (event) => {
+        let interimTranscription = '';
+        let finalTranscription = '';
+
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+            finalTranscription += transcript;
+        } else {
+            interimTranscription += transcript;
+        }
+        }
+
+        setTranscription(finalTranscription);
+        sendTranscription(finalTranscription); // send transcription to Flask server
+    };
+
+    recognition.onerror = (event) => {
+        console.error(event);
+    };
+
+    const startTranscription = () => {
+        recognition.start();
+    };
+
+    const stopTranscription = () => {
+        recognition.stop();
+    };
+
+    const sendTranscription = async (transcription) => {
+        try {
+        const response = await axios.post('http://localhost:5000/transcription', {
+            transcription: transcription
+        });
+        console.log(response);
+        } catch (error) {
+        console.error(error);
+        }
+    };
+
     return (
         <div>
             <Heading> AI Generator </Heading>
             <Text m={20}> Lorem ipsum</Text>
             <Box sx={boxStyles}>
-                Placeholder lorem ipsum
+                {transcription}
             </Box>
-            <Input placeholder='Search here.....'></Input> <IconButton icon="{<Mic/>}"></IconButton>
+            <Button onClick={startTranscription}>Push To Talk</Button>
+            <Text m={20}>Output data: {serverData}</Text>
         </div>
     )
 }
