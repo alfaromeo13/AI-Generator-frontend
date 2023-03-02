a792b6c82be1f16c43ae1e74ac3b7a5a1e55e6ae
Prince Caleb <akakpo.c.benjamin@gmail.com>
2023-03-02 09:11:39 +0000

    Login page updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,23 @@ import { useState, useEffect } from 'react';
 import BgAuth from "../assets/bg-auth.png"
 import { Link } from "react-router-dom"
 import Logo from "../components/Logo" 
+import axios from 'axios';
 
 export function Login(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      localStorage.setItem('token', response.data.token);
+      window.location.reload(); // Refresh the page to apply authenticated changes
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
 
     return (
         <div className="flex overflow-x-hidden bg-auth bg-cover	min-h-screen flex justify-center">
@@ -13,14 +28,15 @@ export function Login(){
             <Logo />
             <div className="content w-3/12 m-36 z-50">
                 <h1 className="text-4xl text-left leading-tight text-dark-blue flex-grow my-4">Welcome back!</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
-                        <input type="email" id="email" className="bg-white/70 w-full py-4 px-8 my-2 border-2 border-white/50 rounded-full" placeholder="Email" required />
+                        <input type="email" value={email} id="email" onChange={event => setEmail(event.target.value)} className="bg-white/70 w-full py-4 px-8 my-2 border-2 border-white/50 rounded-full" placeholder="Email" required />
                     </div> 
                     <div>
-                        <input type="password" className="bg-white/70 w-full py-4 px-8 my-2 border-2 border-white/50 rounded-full" placeholder="Password" required />
+                        <input type="password" value={password} onChange={event => setPassword(event.target.value)} className="bg-white/70 w-full py-4 px-8 my-2 border-2 border-white/50 rounded-full" placeholder="Password" required />
                     </div> 
                     <button type="submit" className="bg-lavender w-full py-4 px-8 my-4 text-xl text-white rounded-full drop-shadow-2xl">Sign In</button>
+                    {error && <div>{error}</div>}
                 </form>
             </div>
         </div>
